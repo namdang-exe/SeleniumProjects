3442ad804c49505e329301ccfd01e2a2a816670a
Dang <Nam.Dang@adph.state.al.us>
2021-09-10 13:17:34 -0500

    Guessing number game

--- GuessingGame.py
+++ GuessingGame.py
@@ -4,5 +4,21 @@ print("What is your name?")
 name = input()
 print(f"Hi {name}, I'm thinking of a number between 1 and 20.")
 secret_number = random.randint(1, 20)
-print("Take a guess.")
-user_guess = int(input())
+index = 0
+while True:
+    index += 1
+
+    print("Take a guess.")
+    user_guess = int(input())
+    if user_guess == secret_number:
+        print(f"Your guess is correct. The number is {secret_number}.")
+        break
+    elif index == 6:
+        print(f"Nope. The number I was thinking of was {secret_number}")
+        break
+    elif user_guess < secret_number:
+        print("Your guess is too low.")
+    elif user_guess > secret_number:
+        print("Your guess is too high")
+
+print(f"You took {index} guesses.")
